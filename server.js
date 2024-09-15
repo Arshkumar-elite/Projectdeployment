4689db16ed0d26208440bd4d3c58293f412fe424
Arsh Kumar <139031872+Arshkumar-elite@users.noreply.github.com>
2024-09-15 16:09:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(
         saveUninitialized: false,
         cookie: {
             httpOnly: true,
-            secure: true,
+            secure: false,
             maxAge: 24 * 60 * 60 * 1000,
         },
     })
