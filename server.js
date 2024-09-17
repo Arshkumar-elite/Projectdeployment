af178390d81164313426af1c49d0a8f6fa7bdb17
Arsh Kumar <139031872+Arshkumar-elite@users.noreply.github.com>
2024-09-17 23:22:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ passport.use(
         {
             clientID: 'Ov23lidheFid8KjRVhFJ',
             clientSecret: 'e16f128e7b7fbf2f18a3c25375461df496ad35a5',
-            callbackURL: 'https://projectdeployment-nu.vercel.app/auth/github/callback',
+            callbackURL: 'https://projectdeployment-steel.vercel.app/auth/github/callback',
         },
         async function (accessToken, refreshToken, profile, cb) {
             try {
